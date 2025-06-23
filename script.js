1ce11f8f27a415d9d646c3340b9ea0fab10d7167
GugaToca <gustavotocalino08@outlook.com>
2025-06-23 18:30:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Futuramente: carregar versículo do dia dinamicamente
+console.log("Bem-vindo ao Clube da Bíblia Kids!");
+
+function mostrarMensagem(mensagem, elementoId, velocidade = 50) {
+  const elemento = document.getElementById(elementoId);
+  elemento.textContent = "";
+  let i = 0;
+} 
